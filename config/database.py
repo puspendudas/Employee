59b9c20e78ba9816874de309fd85f3f4d28e4c79
Puspendu <puspendudas76@gmail.com>
2022-06-01 23:08:33 +0530

    Update Basic

--- config/database.py
+++ config/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL='mysql+mysqlconnector://sk33jnnnpvcz37jm:e5m21wwv2e5ysgb6@spryrr1myu6oalwl.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/a0o2yx84w0seyiax'
+SQLALCHEMY_DATABASE_URL='mysql+mysqlconnector://lxtl8o337juikgmr:uzh45473fs2mvmdd@i54jns50s3z6gbjt.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/cgiivm4du9nulc1n'
 
-SQLALCHEMY_DATABASE_URL='mysql+mysqlconnector://root@localhost:3306/employee'
+# SQLALCHEMY_DATABASE_URL='mysql+mysqlconnector://root@localhost:3306/employee'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
